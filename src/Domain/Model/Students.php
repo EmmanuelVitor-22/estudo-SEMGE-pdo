bdf6ba20a046e53412307a13f40992a98a2590f2
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-07-21 09:18:26 -0300

    feat: cria classe de students

---
+++ src/Domain/Model/Students.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Emmanuel\Domain\Model;
+
+class Students
+{
+    private ?int $id;
+    private string $name;
+    private \DateTimeInterface $birthDate;
+
+    public function __construct(?int $id, string $name, \DateTimeInterface $birthDate)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->birthDate = $birthDate;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getBirthDate(): \DateTimeInterface
+    {
+        return $this->birthDate;
+    }
+
+    public function setBirthDate(\DateTimeInterface $birthDate): void
+    {
+        $this->birthDate = $birthDate;
+    }
+
+
+
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'emmanuelvitor/estudo-semge-pdo',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1860176a6c360dcd5391ed7f4fba995e025064e5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'emmanuelvitor/estudo-semge-pdo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1860176a6c360dcd5391ed7f4fba995e025064e5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
