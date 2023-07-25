2fdacb6274372254b4e1076bbba41acc529589b4
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-07-25 11:02:15 -0300

    feat: usando o parametro nomeado e bindValues, com objetivo de previnir contra SQL Injection

--- insert-pdo.php
+++ insert-pdo.php
@@ -5,8 +5,20 @@ use Emmanuel\Domain\Model\Students;
 require 'vendor/autoload.php';
 $database =  __DIR__ . '/database.sqlite';
 $pdo = new PDO('sqlite:' . $database);
-
 $student = new Students(null,"Juc",new DateTimeImmutable("2000-12-06"));
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->getName()}','{$student->getBirthDate()->format('Y-m-d')}');";
-$result = $pdo->exec($sqlInsert);
-var_dump($result);
+// forma simples de inserir
+//$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->getName()}','{$student->getBirthDate()->format('Y-m-d')}');";
+//$result = $pdo->exec($sqlInsert);
+//var_dump($result);
+
+// Usando SQL Injection
+//forma simples
+$insert = "INSERT INTO students (name, birth_date) VALUES (?,?)";
+//forma nomeada
+//$insert = "INSERT INTO students (name, birth_date) VALUES (:name,:birth_date)";
+
+$statement = $pdo->prepare($insert);
+$statement->bindValue(1,$student->getName());
+$statement->bindValue(2,$student->getBirthDate());
+$statement->execute();
+
