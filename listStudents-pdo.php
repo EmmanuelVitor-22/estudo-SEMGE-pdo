43fa01a81801e1f9c24ad01834fe8721b141bec7
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-07-25 10:05:34 -0300

    feat: cria demonstração de como funciona a forma de busca com fetcAll e somente com fech(para não comprometer memoria quando houver muitos registros no banco)

--- listStudents-pdo.php
+++ listStudents-pdo.php
@@ -7,10 +7,23 @@
     $result = $pdo->query('SELECT * FROM students');
     //Busca os dados inseridos no banco de dados retornando como um array associativo
     $resultDataList = $result->fetchAll(PDO::FETCH_ASSOC);
+
     //mapeando os dados do banco de dados para objetos em siinstanciando a partir do resultado
     $resultStudentList = [];
     foreach ($resultDataList as $itemList) {
-        $resultStudentList[] = new Students($itemList["id"], $itemList["name"], new DateTimeImmutable($itemList["birth_date"]));
-
+        $resultStudentList[] = new Students($itemList["id"],
+                                            $itemList["name"],
+                                            new DateTimeImmutable($itemList["birth_date"])
+        );
         print_r($resultStudentList);
     }
+
+    //EXECUTANDO APENAS UM POR VEZ (evitar esturar memoria quando tem diversos registros)
+    //while ($resultStudent = $result->fetch(PDO::FETCH_ASSOC)){
+    //    $student = new Students($resultStudent["id"],
+    //                            $resultStudent["name"],
+    //                            new DateTimeImmutable($resultStudent["birth_date"])
+    //    );
+    //    print_r($student->getId()); echo PHP_EOL;
+    //}
+    //exit();
